bc3ac2e3ddc2492c71b46fd8f8da7c12c5b0bcc3
ZhenyuLuoo <55060796+ZhenyuLuoo@users.noreply.github.com>
2020-09-30 23:07:30 -0400

    Add files via upload
    Week 1

---
+++ Events/app.js
@@ -0,0 +1,33 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: '',
+      confirmedName: ''
+    };
+  },
+
+  methods: {
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
+    },
+
+    add(num) {
+      this.counter = this.counter + num;
+    },
+
+    reduce(num) {
+      this.counter = this.counter - num;
+    },
+
+    submitForm() {
+      alert('Submitted!');
+    },
+
+    confirmInput() {
+      this.confirmedName = this.name
+    }
+  }
+});
+
+app.mount('#events');
