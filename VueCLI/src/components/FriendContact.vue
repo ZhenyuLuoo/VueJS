9ab248428bab4935d4d45daed4ab8ff7669436a2
ZhenyuLuoo <55060796+ZhenyuLuoo@users.noreply.github.com>
2020-10-14 15:45:27 -0400

    Add files via upload

---
+++ VueCLI/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong>{{ friend.name }}</li>
+            <li><strong>E-mail:</strong>{{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: 'Luo',
+                name: 'Lucas Luo',
+                phone: '980 298 4876',
+                email: 'luoz@appstate.edu'
+            }
+        }
+    },
+
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+};
+
+</script>
